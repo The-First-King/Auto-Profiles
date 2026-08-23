b3d16157f20f34edc1dc60a761697b4f22863b47
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-08-23 23:54:00 +0000

    Update BootCompletedReceiver.java

--- app/src/main/java/com/mine/autoprofile/receivers/BootCompletedReceiver.java
+++ app/src/main/java/com/mine/autoprofile/receivers/BootCompletedReceiver.java
@@ -3,16 +3,19 @@ package com.mine.autoprofile.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
-import com.mine.autoprofile.services.ProfileManagerService;
+import android.os.Build;
+import com.mine.autoprofile.services.TriggerMonitorService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent serviceIntent = new Intent(context, ProfileManagerService.class);
-            context.startForegroundService(serviceIntent);
+            Intent serviceIntent = new Intent(context, TriggerMonitorService.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent);
+            } else {
+                context.startService(serviceIntent);
+            }
         }
     }
 }
