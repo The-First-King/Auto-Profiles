4dcaffeafa1d3de54dcb58878f9bd5287c4a4797
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-08-23 23:51:18 +0000

    Update TriggerDao.java

--- app/src/main/java/com/mine/autoprofile/database/TriggerDao.java
+++ app/src/main/java/com/mine/autoprofile/database/TriggerDao.java
@@ -1,40 +1,16 @@
 package com.mine.autoprofile.database;
 
-import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
-
 import com.mine.autoprofile.models.Trigger;
-
 import java.util.List;
 
 @Dao
 public interface TriggerDao {
+    @Query("SELECT * FROM triggers WHERE type = :type")
+    List<Trigger> getTriggersByType(String type);
 
     @Insert
-    void insert(Trigger trigger);
-
-    @Update
-    void update(Trigger trigger);
-
-    @Delete
-    void delete(Trigger trigger);
-
-    @Query("SELECT * FROM triggers WHERE ruleId = :ruleId")
-    LiveData<List<Trigger>> getTriggersByRuleId(String ruleId);
-
-    @Query("SELECT * FROM triggers WHERE id = :triggerId")
-    LiveData<Trigger> getTriggerById(String triggerId);
-
-    @Query("SELECT * FROM triggers WHERE type = :type")
-    LiveData<List<Trigger>> getTriggersByType(int type);
-
-    @Query("DELETE FROM triggers WHERE ruleId = :ruleId")
-    void deleteTriggersForRule(String ruleId);
-
-    @Query("DELETE FROM triggers")
-    void deleteAll();
+    long insert(Trigger trigger);
 }
