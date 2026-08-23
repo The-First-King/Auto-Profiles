838fee60d0042bb2e83b313913e132c743872bea
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-08-23 23:55:38 +0000

    Update MainActivity.java

--- app/src/main/java/com/mine/autoprofile/ui/MainActivity.java
+++ app/src/main/java/com/mine/autoprofile/ui/MainActivity.java
@@ -1,46 +1,19 @@
 package com.mine.autoprofile.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
-
 import com.mine.autoprofile.R;
+import com.mine.autoprofile.services.TriggerMonitorService;
 
 public class MainActivity extends AppCompatActivity {
-
-    private SwitchCompat masterSwitch;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeViews();
-        setupListeners();
-    }
-
-    private void initializeViews() {
-        masterSwitch = findViewById(R.id.master_switch);
-    }
-
-    private void setupListeners() {
-        masterSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                onMasterSwitchToggled(isChecked);
-            }
-        });
-    }
-
-    private void onMasterSwitchToggled(boolean enabled) {
-        if (enabled) {
-            // Start profile monitoring service
-            // startService(new Intent(this, ProfileManagerService.class));
-        } else {
-            // Stop profile monitoring service
-            // stopService(new Intent(this, ProfileManagerService.class));
-        }
+        // Start background monitoring service when app opens
+        Intent serviceIntent = new Intent(this, TriggerMonitorService.class);
+        startService(serviceIntent);
     }
 }
