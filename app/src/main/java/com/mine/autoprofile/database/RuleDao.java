acff310255b6d527569745f4f5d6fb587d04bbc6
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-08-26 00:32:11 +0000

    Update RuleDao.java

--- app/src/main/java/com/mine/autoprofile/database/RuleDao.java
+++ app/src/main/java/com/mine/autoprofile/database/RuleDao.java
@@ -3,13 +3,16 @@ package com.mine.autoprofile.database;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import com.mine.autoprofile.models.Rule;
+import com.mine.autoprofile.models.FullRule;
 import java.util.List;
 
 @Dao
 public interface RuleDao {
-    @Query("SELECT * FROM rules WHERE enabled = 1")
-    List<Rule> getEnabledRules();
+    @Transaction
+    @Query("SELECT * FROM rules")
+    List<FullRule> getAllRulesWithDetails();
 
     @Insert
     long insert(Rule rule);
