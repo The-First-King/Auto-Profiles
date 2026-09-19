8936a03b4025e21468659b29b592124a174a2ff7
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-09-19 10:15:02 -0400

    Create ProfileManager.java

---
+++ app/src/main/java/android/app/ProfileManager.java
@@ -0,0 +1,18 @@
+package android.app;
+
+import android.content.Context;
+
+public class ProfileManager {
+
+    public static final String PROFILE_SERVICE = "profile";
+
+    public ProfileManager(Context context) {}
+
+    public Profile getActiveProfile() {
+        return null;
+    }
+
+    public void setActiveProfile(String profileName) {
+        // no-op
+    }
+}
