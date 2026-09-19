5cddb4e82233882ba8c59d8379d634a96bde8fe4
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-09-19 10:15:27 -0400

    Create Profile.java

---
+++ app/src/main/java/android/app/Profile.java
@@ -0,0 +1,14 @@
+package android.app;
+
+public class Profile {
+
+    private final String name;
+
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
