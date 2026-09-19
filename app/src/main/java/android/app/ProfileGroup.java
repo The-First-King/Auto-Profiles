b8bdfb72c5186c55e801021c7479f0f96e38ba89
The-First-King <59452275+The-First-King@users.noreply.github.com>
2026-09-19 10:15:50 -0400

    Create ProfileGroup.java

---
+++ app/src/main/java/android/app/ProfileGroup.java
@@ -0,0 +1,14 @@
+package android.app;
+
+public class ProfileGroup {
+
+    private final String name;
+
+    public ProfileGroup(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
